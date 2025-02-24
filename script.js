78efc50a474623fcadc235534dca675483cf646a
Feodor <mr.fedor.kiselev@gmail.com>
2025-02-24 22:10:41 +0300

    tab fix

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log(`ba${2 / undefined}a`)
+console.log(`ba${2 / undefined}a`);
 
 const decimalToBinary = (input) => {
-    if (input === 0 || input === 1) {
-      return String(input);
-    } else {
-      return decimalToBinary(Math.floor(input / 2)) + (input % 2);
-    }
-  };
-  
-  let userNum = Number(prompt("Enter a decimal number"));
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
 
-  console.log(decimalToBinary( userNum ));
-  alert(decimalToBinary( userNum ));
+let userNum = Number(prompt("Enter a decimal number"));
+
+console.log(decimalToBinary(userNum));
+alert(decimalToBinary(userNum));
